5127f788d68ff41d6a882d502ee9431eab1c71af
zbashay <ronatorino@gmail.com>
2020-01-20 22:34:25 -0600

    mmm

--- src/exercises.js
+++ src/exercises.js
@@ -2,7 +2,7 @@ function commonEnd(a, b) {
   if (!a || a.length === 0 || !b || b.length === 0) {
     return false;
   }
-
+  
   if (a[0] === b[0] || (a[a.length-1] === b[b.length-1])) {
     return true;
   }
